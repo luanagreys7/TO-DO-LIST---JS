5f42a068e67b30bfbfea32ebcff1c39177cc5acb
luanagreys7 <luanagreys7@gmail.com>
2024-11-24 18:22:37 -0300

    TESTE ADD TASK

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-alert("teste")
 // Seleção de elementos
 const todoForm = document.querySelector("#todo-form");
 const todoInput = document.querySelector("#newtask");
@@ -8,8 +7,55 @@ const editInput = document.querySelector("#edit-input");
 const cancelEditBtn = document.querySelector("#cancel-edit-btn");
 
 // Funções
+const addTodo = (text) => {
+
+    const todo = document.createElement("div")
+    todo.classList.add("todo")
+
+    const todoTitle = document.createElement("h3")
+    todoTitle.innerText = text
+    todo.appendChild(todoTitle)
+
+    //Finish
+    const_doneBtn = document.createElement("button");
+    doneBtn.classList.add("finish-todo");
+    doneBtn.innerHTML = '<i class="fa-solid fa-check"></i>';
+    todo.appendChild(doneBtn);
+    //Finish
+
+    //Editar
+    const_doneBtn = document.createElement("button");
+    doneBtn.classList.add("edit-todo");
+    doneBtn.innerHTML = '<i class="fa-solid fa-pen"></i>';
+    todo.appendChild(doneBtn);
+    //Editar
+
+    //Remover
+    const_doneBtn = document.createElement("button");
+    doneBtn.classList.add("remove-todo");
+    doneBtn.innerHTML = '<i class="fa-solid fa-trash"></i>';
+    todo.appendChild(doneBtn);
+    //Remover
+
+    //Ver descrição
+    const_doneBtn = document.createElement("button");
+    doneBtn.classList.add("see-description");
+    doneBtn.innerHTML = '<i class="fa-solid fa-eye"></i>';
+    todo.appendChild(doneBtn);
+    //Ver descrição
+
+    todoList.appendChild(todo);
+
+    todoInput.value = "";
+};
 
 // Eventos
 todoForm.addEventListener('submit', function(e){
-    alert("teste")
+    e.preventDefault(); //prevents the page from reloading automatically after submit. 
+    const inputValue = todoInput.value.trim(); //guarda o valor do input do usuário. trim removes unwanted space.
+    if(inputValue.lenght > 0){
+        addTodo(inputValue)
+    }
+   
+
 });
